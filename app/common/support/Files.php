e8870b870f3b553f9c17060b0ff718bf87d52a3b
mac <mac@192.168.9.40>
2023-07-04 16:51:43 +0800

    加入文件上传方法

--- app/common/support/Files.php
+++ app/common/support/Files.php
@@ -3,16 +3,28 @@
 namespace app\common\support;
 
 use app\common\traits\ResponseJson;
+use think\facade\Request;
 
 class Files
 {
     use ResponseJson;
-    public function upload($name='file'){
+
+    /**
+     * 上传文件
+     * @param $name file
+     * @return \think\response\Json
+     */
+    public function upload($name = 'file'): \think\response\Json
+    {
         $file = request()->file($name);
+        $domain = Request::domain(true);
         // 上传到本地服务器
-        $saveName = \think\facade\Filesystem::disk('public')->putFile( 'topic', $file, 'md5');
+        $saveName = \think\facade\Filesystem::disk('public')->putFile('topic', $file, 'md5');
+        $url = \think\facade\Filesystem::disk('public')->url($saveName);
+        // 在控制器或路由闭包函数中使用
         return $this->success('', [
             'path' => $saveName,
+            'url'=>$domain.$url
         ]);
     }
 }
